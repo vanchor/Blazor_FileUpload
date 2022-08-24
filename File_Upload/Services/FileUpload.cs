d9b58ce62db7f057b0bf729fd4886a94be439593
vanchor <vovanyakorev2@gmail.com>
2022-08-24 21:04:20 +0200

    first commit

---
+++ File_Upload/Services/FileUpload.cs
@@ -0,0 +1,43 @@
+﻿using Microsoft.AspNetCore.Components.Forms;
+
+namespace File_Upload.Services
+{
+    public interface IFileUpload
+    {
+        Task UpLoadFile(IBrowserFile file);
+    }
+    public class FileUpload : IFileUpload
+    {
+        private IWebHostEnvironment _webHostEnvironment;
+        private readonly ILogger<FileUpload> _logger;
+
+        public FileUpload(IWebHostEnvironment webHostEnvironment, ILogger<FileUpload> logger)
+        {
+            _webHostEnvironment = webHostEnvironment;
+            _logger = logger;
+        }
+
+        public async Task UpLoadFile(IBrowserFile file)
+        {
+            if(file is not null)
+            {
+                try
+                {
+                    var uploadPath = Path.Combine(_webHostEnvironment.WebRootPath, "uploads", file.Name);
+                    using (var stream = file.OpenReadStream())
+                    {
+                        var fileStram = File.Create(uploadPath);
+                        await stream.CopyToAsync(fileStram);
+                        fileStram.Close();
+
+                    }
+
+                } 
+                catch (Exception ex)
+                {
+                    _logger.LogError(ex.ToString());
+                }
+            }
+        }
+    }
+}
