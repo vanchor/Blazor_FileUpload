1fcd617303e079ad3e75128752c080c5821f6672
vanchor <vovanyakorev2@gmail.com>
2022-08-27 12:17:58 +0200

    Added preview to selected imgs

--- File_Upload/Services/FileUpload.cs
+++ File_Upload/Services/FileUpload.cs
@@ -5,6 +5,7 @@ namespace File_Upload.Services
     public interface IFileUpload
     {
         Task UpLoadFile(IBrowserFile file);
+        Task<string> GeneratePreviewUrl(IBrowserFile file);
     }
     public class FileUpload : IFileUpload
     {
@@ -38,6 +39,22 @@ namespace File_Upload.Services
                     _logger.LogError(ex.ToString());
                 }
             }
+        }
+
+        public async Task<string> GeneratePreviewUrl(IBrowserFile file)
+        {
+            if (!file.ContentType.Contains("image"))
+            {
+                if (file.ContentType.Contains("pdf"))
+                    return "images/pdf_logo.png";
+
+            }
+
+            var resizedImage = await file.RequestImageFileAsync(file.ContentType, 100, 100);
+            var buffer = new byte[resizedImage.Size];
+            await resizedImage.OpenReadStream().ReadAsync(buffer);
+            return $"data:{file.ContentType};base64,{Convert.ToBase64String(buffer)}";
+
         }
     }
 }
